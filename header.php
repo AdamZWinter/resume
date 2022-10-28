3584d07a4fa363392eda948b4bf34086d6be5b1b
AdamZWinter <adamwint@srv604.webhostingforstudents.com>
2022-10-28 13:03:50 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,73 @@
+<?php
+//You do not have to echo html content.  It can just sit outside the php script.
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="bootstrap5.css" rel="stylesheet">
+  <link href="adam.css" rel="stylesheet">
+
+  <style>
+
+  </style>
+
+<title>Resume for Adam Winter</title>
+</head>
+
+<body>
+  <div class = " container-fluid">
+  <div class= "row">
+    <div class="d-sm-none d-md-block col-1">
+              <!--
+                <h1>Left Margin</h1>
+              -->
+    </div>
+    <div class="col-5 col-sm-12"><h1 class="myHeader-Title">Adam Winter</h1></div>
+    <div class = "col-5 col-sm-12 logo"><img class= "img-logo myLogo float-end" src="img/fromHank.bmp" alt="Bootstrap Music Logo"></div>
+
+    <div class="d-sm-none d-md-block col-1">
+              <!--
+                <h1>Right Margin</h1>
+              -->
+    </div>
+  </div>
+  </div>
+
+    <!--
+    ********************************* NAV BAR ****************************************************************************************************
+  -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <!--a class="navbar-brand" href="#">Navbar</a-->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="https://adamwinter.greenriverdev.com/resume/">Adam Winter</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    More
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="teamAgreement.php">......</a></li>
+                    <li><a class="dropdown-item" href="check-inForm.php">.....</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="more.php">.....</a></li>
+                  </ul>
+                </li>
+                <!--li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li-->
+              </ul>
+            </div>
+          </div>
+        </nav>
+  <!--
+    ********************************* End of  NAV BAR ****************************************************************************************************
+  -->
